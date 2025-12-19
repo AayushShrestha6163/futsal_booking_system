2804b05b19a3bb57a6b47001cb37f3c836c2d43b
AayushShrestha6163 <aayushshrestha6163@gmail.com>
2025-12-19 14:08:35 +0545

    app dart updated

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:futal_booking_system/screen/splash_screen.dart';
+import 'package:futal_booking_system/theme/theme_data.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,7 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: getApplicationTheme(), 
       home: const SplashScreen(),
     );
   }
