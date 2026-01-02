e388e5cba5e3c3841e4ac960185695e0b19ce9b6
AayushShrestha6163 <aayushshrestha6163@gmail.com>
2026-01-02 21:31:51 +0545

    mybutton.dart and my text foem field.dart file path changed

---
+++ lib/core/widget/my_textformfield.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MyTextFormField extends StatelessWidget {
+  const MyTextFormField({
+    super.key,
+    required this.label,
+    this.controller,
+    this.onChanged,
+    this.validator,
+    this.keyboardType = TextInputType.text,
+    this.obscureText = false,
+  });
+
+  final String label;
+  final TextEditingController? controller;
+  final ValueChanged<String>? onChanged;
+  final String? Function(String?)? validator;
+  final TextInputType keyboardType;
+  final bool obscureText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      onChanged: onChanged,
+      validator: validator,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        labelText: label,
+      ),
+    );
+  }
+}
